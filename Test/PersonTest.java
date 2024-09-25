74b805a80966477b7dd41541ce313c09b37d9b92
tirthpatel2184@gmail.com <tirthpatel2184@gmail.com>
2024-09-25 15:33:04 -0400

    First Commit for lab 4

---
+++ Test/PersonTest.java
@@ -0,0 +1,8 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+
+    private String firstName;
+
+    private String lastName;
+
+    public String setName(String firstName, String lastName) {
+        return firstName + " " + lastName;
+    }
+}
+
+
